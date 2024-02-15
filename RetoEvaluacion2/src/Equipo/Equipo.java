f08f9aa223f708f4a15e9f29cae9591d93f9d6c1
Alderoto16 <aldersanchez163@gmail.com>
2024-02-15 14:10:10 +0100

    Pepe

--- RetoEvaluacion2/src/Equipo/Equipo.java
+++ RetoEvaluacion2/src/Equipo/Equipo.java
@@ -5,8 +5,55 @@ import Entrenamiento.Entrenamiento;
 
 public class Equipo {
 
-	private String equipo_nombre;
+	private String nombreEquipo;
 	private String estadio;
-	private ArrayList<Entrenamiento> entraList;
+	private int titulos;
+	private ArrayList<Entrenamiento> listaEntrenamiento;
+
+	public Equipo() {
+		this.nombreEquipo = "";
+		this.estadio = "";
+		this.listaEntrenamiento = null;
+		this.titulos = 0;
+	}
+
+	public Equipo(String nombreEquipo, String estadio, int titulos, ArrayList<Entrenamiento> listaEntrenamiento) {
+		this.nombreEquipo = nombreEquipo;
+		this.estadio = estadio;
+		this.titulos = titulos;
+		this.listaEntrenamiento = listaEntrenamiento;
+	}
+
+	public String getNombreEquipo() {
+		return nombreEquipo;
+	}
+
+	public void setNombreEquipo(String nombreEquipo) {
+		this.nombreEquipo = nombreEquipo;
+	}
+
+	public String getEstadio() {
+		return estadio;
+	}
+
+	public void setEstadio(String estadio) {
+		this.estadio = estadio;
+	}
+
+	public int getTitulos() {
+		return titulos;
+	}
+
+	public void setTitulos(int titulos) {
+		this.titulos = titulos;
+	}
+
+	public ArrayList<Entrenamiento> getListaEntrenamiento() {
+		return listaEntrenamiento;
+	}
+
+	public void setListaEntrenamiento(ArrayList<Entrenamiento> listaEntrenamiento) {
+		this.listaEntrenamiento = listaEntrenamiento;
+	}
 
 }
