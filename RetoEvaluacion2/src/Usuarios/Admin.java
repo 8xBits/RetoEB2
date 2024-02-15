999e5e331a4707561f80210dc7b3342cb33d56ab
Alderoto16 <aldersanchez163@gmail.com>
2024-02-15 13:58:13 +0100

    Pepe

--- RetoEvaluacion2/src/Usuarios/Admin.java
+++ RetoEvaluacion2/src/Usuarios/Admin.java
@@ -1,5 +1,24 @@
 package Usuarios;
 
-public class Admin extends Usuarios{
+public class Admin extends Usuarios {
+	private int codAdmin;
+
+	public Admin(String nombre, String user, String contraseña, int codAdmin) {
+		super(nombre, user, contraseña);
+		this.codAdmin = codAdmin;
+	}
+
+	public Admin() {
+		super();
+		this.codAdmin = 0;
+	}
+
+	public int getCodAdmin() {
+		return codAdmin;
+	}
+
+	public void setCodAdmin(int codAdmin) {
+		this.codAdmin = codAdmin;
+	}
 
 }
--- RetoEvaluacion2/src/Usuarios/Entrenador.java
+++ RetoEvaluacion2/src/Usuarios/Entrenador.java
@@ -1,5 +1,35 @@
 package Usuarios;
 
-public class Entrenador extends Usuarios{
+public class Entrenador extends Usuarios {
+	private String nombreEquipo;
+	private CargoEntrenador cargo;
+
+	public Entrenador(String nombre, String user, String contraseña, String nombreEquipo, CargoEntrenador cargo) {
+		super(nombre, user, contraseña);
+		this.nombreEquipo = nombreEquipo;
+		this.cargo = cargo;
+	}
+
+	public Entrenador() {
+		super();
+		this.nombreEquipo = "";
+		this.cargo = null;
+	}
+
+	public String getNombreEquipo() {
+		return nombreEquipo;
+	}
+
+	public void setNombreEquipo(String nombreEquipo) {
+		this.nombreEquipo = nombreEquipo;
+	}
+
+	public CargoEntrenador getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(CargoEntrenador cargo) {
+		this.cargo = cargo;
+	}
 
 }
