eb607a01a0b08243ed2dadd8367f8871ec162dbd
8xBits <multitartanga@gmail.com>
2024-02-16 10:09:19 +0100

    ch

--- RetoEvaluacion2/src/Main/Main.java
+++ RetoEvaluacion2/src/Main/Main.java
@@ -1,10 +1,14 @@
 package Main;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 import Equipo.Equipo;
 import Entrenamiento.Entrenamiento;
 import Usuarios.*;
@@ -173,10 +177,10 @@ public class Main {
 			opc = Util.leerInt();
 			switch (opc) {
 			case 1:
-				comprobarDorsal();
+				comprobarDorsal(fich);
 				break;
 			case 2:
-				verInfoEquipo();
+				verInfoEquipo(fich);
 				break;
 			case 3:
 				System.out.println("Hasta pronto..");
@@ -186,12 +190,89 @@ public class Main {
 		} while (opc != 3);
 	}
 
-	public static void comprobarDorsal() {
+	public static void comprobarDorsal(File fich) {
+		int choice;
+		Jugador jugador= new Jugador();
+		Equipo equipo= new Equipo();
+		ArrayList<Integer> dorsalNoLibre= new ArrayList<>();
+		ArrayList<Jugador> jugadorList= new ArrayList<>();
+			fileToArray(fich,jugadorList);
+		for(Jugador jug :jugadorList) {
+			if(jug.getNombreEquipo().equalsIgnoreCase(equipo.getNombreEquipo())) {
+				dorsalNoLibre.add(jug.getDorsal());
+			}
+		}
+		System.out.println("Dorsales Libres :  " );
+		for(int i=0;i<26;i++) {
+			if(!dorsalNoLibre.contains(i)) {
+				System.out.println(i+" " );
+			}
+		}
+		
+		do {
+			System.out.println("Elege un dorsal libre que te gusta :");
+			choice = Util.leerInt();
+		} while (dorsalNoLibre.contains(choice) );
 
 	}
 
-	public static void verInfoEquipo() {
+	public static void verInfoEquipo(File fich) {
+		ArrayList<Equipo> equipoList= new ArrayList<>();
+		Equipo equipo= new Equipo();
+		fileToArray(fich,equipoList);
+		for(Equipo equip :equipoList) {
+			if(equip.getNombreEquipo().equalsIgnoreCase(equipo.getNombreEquipo())) {
+				equip.getDatosEquipo();
+			}
 			
+		}
+
 	}
+	
+	  private static void arrayToFile(ArrayList<Jugador> empList, File fich) {
+	        ObjectOutputStream oos = null;
+	        try {
+	            if (fich.exists()) {
+	                fich.delete();
+	            }
+	            oos = new ObjectOutputStream(new FileOutputStream(fich));
+	            for (Jugador emp : empList) {
+	                oos.writeObject(emp);
+	            }
+	            empList.clear();
+	            oos.close();
+
+	        } catch (FileNotFoundException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+	    }
+
+	    private static void fileToArray(File fich, ArrayList<Jugador> empList) {
+	        if (fich.exists()) {
+	            ObjectInputStream ois;
+	            try {
+	                ois = new ObjectInputStream(new FileInputStream(fich));
+	                int cuantos = Util.calculoFichero(fich);
+	                for (int i = 0; i < cuantos; i++) {
+	                	Jugador emp = (Jugador) ois.readObject();
+	                    empList.add(emp);
+	                }
+	              /*  Empleado emp = (Empleado) ois.readObject();
+	                while (emp != null) {
+	                    arrayFich.add(emp);
+	                    emp = (Empleado) ois.readObject();
+	                }*/
+	                ois.close();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            } catch (ClassNotFoundException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
 
 }// class
