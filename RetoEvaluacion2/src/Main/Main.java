7a67500cb727be178d63a7384826968e842ce459
Alderoto16 <aldersanchez163@gmail.com>
2024-02-16 09:51:49 +0100

    Pepe

--- RetoEvaluacion2/src/Main/Main.java
+++ RetoEvaluacion2/src/Main/Main.java
@@ -1,9 +1,12 @@
 package Main;
 
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import Equipo.Equipo;
 import Entrenamiento.Entrenamiento;
@@ -24,7 +27,7 @@ public class Main {
 		do {
 			userType = Util.leerInt("Desea iniciar sesion 3=si/4=no", 3, 4);
 			if (userType == 3) {
-				logIn();
+				logIn(fichUsuarios, userType);
 				int menu = 0;
 				switch (userType) {
 				case 0:
@@ -65,14 +68,30 @@ public class Main {
 		}
 	}
 
-	private static void logIn() {
+	private static int logIn(File fichUsuarios, int userType) {
 		String user, passwd;
+		boolean existe = false;
+		int intentos = 3;
 
-		System.out.println("LOG IN");
-		System.out.print("User: ");
-		user = Util.introducirCadena();
-		System.out.print("Contraseña: ");
-		passwd = Util.introducirCadena();
+		do {
+			System.out.println("LOG IN");
+			System.out.print("User: ");
+			user = Util.introducirCadena();
+			System.out.print("Contraseña: ");
+			passwd = Util.introducirCadena();
+			int realizar = consulta(fichUsuarios, user, passwd);
+			if (realizar == -1) {
+				System.out.println("El usuario y la contraseña introducidos no coinciden o no existen");
+				existe = true;
+				intentos--;
+				System.out.println("Te quedan " + intentos + " intentos");
+			}
+			if (intentos == 0) {
+				return userType = 4;
+			}
+		} while (existe);
+
+		return userType;
 
 	}
 
@@ -165,5 +184,75 @@ public class Main {
 //omar
 	private static void menuJugador() {
 
+	}
+
+	private static int consulta(File fich, String user, String passwd) {
+		ObjectInputStream ois = null;
+
+		try {
+			ois = new ObjectInputStream(new FileInputStream(fich));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return -1; // Manejar el error de archivo no encontrado
+		} catch (IOException e) {
+			e.printStackTrace();
+			return -1; // Manejar otros errores de entrada/salida
+		}
+
+		System.out.println("Introduce el DNI que quieres buscar");
+
+		int index = 0;
+		try {
+			while (true) {
+				Usuarios aux = (Usuarios) ois.readObject();
+				if (aux.getUser().equalsIgnoreCase(user)) {
+					if (aux.getContraseña().equalsIgnoreCase(passwd)) {
+						return index;
+					}
+				}
+				index++;
+			}
+		} catch (EOFException e) {
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ois != null) {
+					ois.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return -1;
+	}
+
+	private static int comprobar(File fichUsuarios, int userType, String user, String passwd) {
+		int tipoUsuario = consulta(fichUsuarios, user, passwd);
+		int contador = 0;
+		ObjectInputStream ois = null;
+
+		try {
+			while (true) {
+				Usuarios aux = (Usuarios) ois.readObject();
+				if (contador == tipoUsuario) {
+
+				}
+			}
+
+		} catch (EOFException e) {
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ois != null) {
+					ois.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return userType;
 	}
 }
