04525f76f2df641f79550594bf2214f2c605b982
Alderoto16 <144679671+Alderoto16@users.noreply.github.com>
2024-02-22 17:38:13 +0100

    Delete RetoEvaluacion2 directory

--- RetoEvaluacion2/src/Utilidades/ExcepcionUser.java
+++
@@ -1,9 +0,0 @@
-package Utilidades;
-
-public class ExcepcionUser extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public ExcepcionUser(String mensaje) {
-		super(mensaje);	
-	}
-}
